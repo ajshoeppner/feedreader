ffd0339b666ff22f66d4ed812168e657a0daa012
Johnathan Niziol <35515+jniziol@users.noreply.github.com>
2019-05-31 10:59:31 -0500

    Update feedreaderSpec.js

--- jasmine/spec/feedreaderSpec.js
+++ jasmine/spec/feedreaderSpec.js
@@ -55,8 +55,7 @@ describe('RSS Feeds', () => {
   /* TODO: Write a test that ensures when the loadFeed
     * function is called and completes its work, there is at least
     * a single .entry element within the .feed container.
-    * Remember, loadFeed() is asynchronous so this test will require
-    * the use of Jasmine's beforeEach and asynchronous done() function.
+    * Remember, loadFeed() is asynchronous.
     */
 
 /* TODO: Write a new test suite named "New Feed Selection" */
